68bd0f929ebae4c9b66c57adc19f02994e9f223a
Ruairi-osul <ruairi.osullivan@univ.ox.ac.uk>
2024-01-30 20:03:09 +0000

    back formatting for repo

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 from setuptools import setup, find_packages
 
+
 def read_requirements():
-    with open('requirements.txt', 'r') as req:
+    with open("requirements.txt", "r") as req:
         content = req.read()
-        requirements = content.split('\n')
+        requirements = content.split("\n")
 
     return requirements
 
+
 setup(
-    name='Operant-Operation',
-    version='0.1',
+    name="Operant-Operation",
+    version="0.1",
     packages=find_packages(),
-    python_requires='>=3.11',
+    python_requires=">=3.11",
     install_requires=read_requirements(),
-    author='Ruairi O\'Sullivan',
-    author_email='ruairi.osullivan.work@gmail.com',
-    description='Analysis for a set of operant conditioning neuroscience experiments.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/Ruairi-osul/operant-operation',
+    author="Ruairi O'Sullivan",
+    author_email="ruairi.osullivan.work@gmail.com",
+    description="Analysis for a set of operant conditioning neuroscience experiments.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ruairi-osul/operant-operation",
 )
